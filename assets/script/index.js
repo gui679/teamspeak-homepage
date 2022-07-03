8d02654d9514e596fcfaeec9e3aa2091c1b57848
Guilherme de Medeiros <guilherme.medeiros@compasso.com.br>
2022-07-03 18:10:58 -0300

    several fixes

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,9 +6,13 @@ const resizeObserver = new ResizeObserver(entries =>
         } else {
             document.querySelector('footer').style = "position:absolute";            
         }
+        document.querySelector("a[href='https://www.tsviewer.com/']").remove();
+        document.querySelector("a[href='https://play.google.com/store/apps/details?id=com.tsviewer.webapp']").remove();
     }
   )
   
   // start observing a DOM node
   resizeObserver.observe(document.body);
   resizeObserver.observe(document.getElementById('ts3viewer_1127191'));
+
+  document.querySelector("a[href='https://www.tsviewer.com/']").remove();
